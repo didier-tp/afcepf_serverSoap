099783fedb96903f77c01b0dc93157d729eff2b6
didier <didier-defrance@wanadoo.fr>
2017-11-13 16:56:19 +0100

    init

--- init_push.bat
+++ init_push.bat
@@ -1,6 +1,7 @@
 cd /d "%~dp0"
-git remote add resaSpringGitHubOrigin https://didier-tp:pwd007!@github.com/didier-tp/serverSoap.git
-git push -u resaSpringGitHubOrigin master
+git remote add gitHubOriginAfcepfServerSoap https://didier-tp:pwd007!@github.com/didier-tp/afcepf_serverSoap.git
+git push -u gitHubOriginAfcepfServerSoap master
+pause
 
 REM open with text editor
 REM opne with system editor
--- src/main/java/fr/afcepf/dja/ws/ServerSoapApp.java
+++ src/main/java/fr/afcepf/dja/ws/ServerSoapApp.java
@@ -6,7 +6,10 @@ public class ServerSoapApp {
 
 	public static void main(String[] args) {
 		ServiceTvaImpl  serviceTvaImpl = new ServiceTvaImpl();
-		String wsUrl = "http://localhost:8080/serverSoap/ws/tva";
+		//NB: si Endpoint.publish() avec url en localhost , la securité par défaut
+		//fait que les appels provenant d'autres machines seront refusés.
+		//String wsUrl = "http://" + "192.168.102.116" +":8080/serverSoap/ws/tva";
+		String wsUrl = "http://" + "localhost" +":8080/serverSoap/ws/tva";
 		//ou bien String wsUrl = "http://localhost:8181/serverSoap/ws/tva";
 		System.out.println("démarrage du serveur , url=" +wsUrl);
 		Endpoint.publish(wsUrl, serviceTvaImpl);
