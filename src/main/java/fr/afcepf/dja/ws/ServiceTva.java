507eaa75da69b95921898935b81fc3c065ce27f6
didier <didier-defrance@wanadoo.fr>
2017-11-14 10:34:39 +0100

    nouvelle version

--- src/main/java/fr/afcepf/dja/ws/ServiceTva.java
+++ src/main/java/fr/afcepf/dja/ws/ServiceTva.java
@@ -3,6 +3,8 @@ package fr.afcepf.dja.ws;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 
+import fr.afcepf.dja.data.ResCalculTva;
+
 @WebService()//avec namespace par defaut : http:// + package java à l'envers + /
              //http://ws.dja.afcepf.fr/
 public interface ServiceTva {
@@ -11,4 +13,6 @@ public interface ServiceTva {
 			          @WebParam(name="taux") double taux);
 	public String getAuteur();
     //...
+	public ResCalculTva calculerTvaEtTtc(@WebParam(name="ht")double ht,
+			                              @WebParam(name="taux") double taux);
 }
--- src/main/java/fr/afcepf/dja/ws/ServiceTvaImpl.java
+++ src/main/java/fr/afcepf/dja/ws/ServiceTvaImpl.java
@@ -2,6 +2,8 @@ package fr.afcepf.dja.ws;
 
 import javax.jws.WebService;
 
+import fr.afcepf.dja.data.ResCalculTva;
+
 @WebService(endpointInterface="fr.afcepf.dja.ws.ServiceTva")
 public class ServiceTvaImpl implements ServiceTva {
 
@@ -13,6 +15,16 @@ public class ServiceTvaImpl implements ServiceTva {
 	@Override
 	public String getAuteur() {
 		return "didier defrance / formateur fou";
+	}
+
+	@Override
+	public ResCalculTva calculerTvaEtTtc(double ht, double taux) {
+		ResCalculTva resCalculTva = new ResCalculTva();
+		resCalculTva.setHt(ht);
+		resCalculTva.setTaux(taux);
+		resCalculTva.setTva(ht*taux/100.0);
+		resCalculTva.setTtc(ht + resCalculTva.getTva());
+		return resCalculTva;
 	}
 
 }
