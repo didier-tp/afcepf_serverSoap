e64bc47457975b3ccfca977e2de107f6c6d4151a
didier <didier-defrance@wanadoo.fr>
2017-11-15 16:03:18 +0100

    avec query rest

--- src/main/java/fr/afcepf/dja/rest/RestDeviseService.java
+++ src/main/java/fr/afcepf/dja/rest/RestDeviseService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -27,9 +29,32 @@ public class RestDeviseService {
 	
 	@GET
 	//url : http://localhost:8080/serverSoap/ws/rest/devise
+	//ou bien http://localhost:8080/serverSoap/ws/rest/devise?tauxChangeMini=1.1
 	@Path("") //ou bien @Path("/") ou bien @Path("/all")
-	public List<Devise> allDevise(){
-		return deviseDao.findAllDevise();
+	public List<Devise> /*allDevise()*/
+	       devisesByCriteria(@QueryParam("tauxChangeMini")Double tauxChangeMini){
+		      List<Devise> listeDevises = deviseDao.findAllDevise();
+		      if(tauxChangeMini!=null){
+		    	  //boucle de n-1 à 0 pour ne pas perturber les indice lors du remove()
+		    	  //si non , bogon = particule élémentaire de bug.
+		    	  for(int i=listeDevises.size()-1;i>=0;i--){
+		    		  Devise d = listeDevises.get(i);
+		    		  if(d.getTauxChange()<tauxChangeMini){
+		    			  listeDevises.remove(d);
+		    		  }
+		    	  }
+		      }
+		      return listeDevises;
 	}
+	@GET
+	//url : http://localhost:8080/serverSoap/ws/rest/devise/EUR
+	@Path("/{codeDev}") 
+	public Devise deviseByCode(@PathParam("codeDev")String codeDevise){
+		return deviseDao.findDeviseByCode(codeDevise);
+	}
+	
+	
+	
+	
 
 }
